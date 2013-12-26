85bc6958fb26686b4e128db9a76ba91fe186f8dd
rguzman <rguzman@robotnik.es>
2013-12-26 09:06:00 +0000

    delete more build files

--- tags/summit_xl_sim_groovy_v1.0/robotnik_msgs/build/devel/lib/python2.7/dist-packages/robotnik_msgs/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._inputs_outputs import *
-from ._encoders import *
-from ._ptz import *
-from ._Axis import *
--- tags/summit_xl_sim_groovy_v1.0/robotnik_msgs/build/devel/share/common-lisp/ros/robotnik_msgs/srv/_package_set_height.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package robotnik_msgs-srv)
-(cl:export '(HEIGHT-VAL
-          HEIGHT
-          RET-VAL
-          RET
-))

8e7a8c72d70c7357e1be1dd99733309ac07ea5d4
rguzman <rguzman@robotnik.es>
2013-12-26 08:58:50 +0000

    added current trunk version - includes x_wam

---
+++ trunk/summit_xl_sim_groovy/summit_xl_waypoints/build/devel/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+CATKIN_SHELL=bash
+. "/home/trurl/catkin_ws/src/summit_xl_sim/summit_xl_waypoints/build/devel/setup.sh"
---
+++ trunk/summit_xl_sim_groovy/summit_xl_waypoints/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/trurl/catkin_ws/src/summit_xl_sim/summit_xl_waypoints/lib/libgtest_main.pdb"
+  "/home/trurl/catkin_ws/src/summit_xl_sim/summit_xl_waypoints/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
